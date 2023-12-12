acb4e2c2617807b109560f20cc40fd2534036151
balamurthy68 <43419549+balamurthy68@users.noreply.github.com>
2023-12-12 14:33:39 +0530

    Add files via upload

---
+++ TakeScreenshot.java
@@ -0,0 +1,30 @@
+package utils;
+
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+
+
+public class TakeScreenshot {
+
+    /**
+     * This function will take screenshot
+     * @param webdriver
+     * @param fileWithPath
+     * @throws Exception     *      */
+    public static void takeSnapShot(WebDriver webdriver,String fileWithPath) throws Exception{
+        //Convert web driver object to TakeScreenshot
+        TakesScreenshot scrShot =((TakesScreenshot)webdriver);
+        //Call getScreenshotAs method to create image file
+        File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+       //Move image file to new destination
+         File DestFile=new File(fileWithPath);
+      //Copy file at destination
+        FileUtils.copyFile(SrcFile, DestFile);
+    }
+
+}
